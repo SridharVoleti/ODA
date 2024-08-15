2a95b7400beac32b64892e27d05f5bac492e04b3
yogesh28600 <yogesh28600@gmail.com>
2024-08-15 21:20:58 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App/templates/**"],
+  theme: {
+    extend: {
+      colors: {
+        "ocean-blue": "#5EB6EE",
+        "ocean-green": "#69b493",
+      },
+    },
+  },
+  plugins: [],
+};
