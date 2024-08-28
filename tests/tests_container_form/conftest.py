5753e0303742fd686fd233f3421007b2edbf7488
Rachana Donthala <donthalarachana@gmail.com>
2024-08-28 19:45:50 +0530

    added the tests for container form

---
+++ tests/tests_container_form/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from app import app as create_app
+
+# Fixture to create the Flask app instance
+@pytest.fixture
+def app():
+    app = create_app
+    app.config['WTF_CSRF_ENABLED'] = False # Disabled CSRF
+    return app
+# Fixture to provide the test client from Flask for making requests
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+# Fixture to establish the application context, allowing access to global objects like current_app
+@pytest.fixture
+def app_context(app):
+    with app.app_context():
+        yield app  # The app context is available during the test
+
+# Fixture to establish a request context, making request-specific variables (like request) available
+@pytest.fixture
+def request_context(app, client):
+    with app.test_request_context():
+        yield  # The request context is available during the test
+@pytest.fixture
+def form_data():
+    return {
+        'container_type' : "20",
+        'container_size' :"10*12*40",
+        'container_weight': 70,
+        'max_gross_weight' : 50,
+        'owner_or_operator_code' : "Rachana2000",
+        'container_status' : "Empty",
+        'iso_code' : "7896",
+        'container_condtition' : "Clean",
+        'date_of_manufacture' : "2024-08-20",
+        'last_date_inspection' : "2025-07-30",
+        'cargo_type' : "General"
+     }
---
+++ tests/tests_container_form/tests_container_form.py
@@ -0,0 +1,90 @@
+from app.forms.container_form import ContainerForm 
+# Test case for validating a form with all required fields provided
+def test_valid_form(request_context,form_data):
+    form = ContainerForm(data=form_data)
+    assert form.validate() is True  # Assert that the form should be valid with the given data
+
+# Test case for validating a form with a invalid container_type
+def test_invalid_container_type(request_context, form_data):
+    form_data['container_type'] = ''
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_type' in form.errors
+
+# Test case for validating a form with an invalid container_size
+def test_invalid_container_size(request_context, form_data):
+    form_data['container_size'] = ''  # Missing one dimension
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_size' in form.errors
+
+# Test case for validating a form with an invalid container_weight (negative value)
+def test_invalid_container_weight_negative(request_context, form_data):
+    form_data['container_weight'] = ''
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_weight' in form.errors
+
+# Test case for validating a form with an invalid container_weight (exceeding max gross weight)
+def test_invalid_container_weight_exceeding(request_context, form_data):
+    form_data['container_weight'] = 'hjderr' 
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_weight' in form.errors
+
+# Test case for validating a form with an invalid max_gross_weight (negative value)
+def test_invalid_max_gross_weight(request_context, form_data):
+    form_data['max_gross_weight'] = ''
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'max_gross_weight' in form.errors
+
+# Test case for validating a form with an invalid owner_or_operator_code (special characters)
+def test_invalid_owner_or_operator_code(request_context, form_data):
+    form_data['owner_or_operator_code'] = ''  
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'owner_or_operator_code' in form.errors
+
+# Test case for validating a form with an invalid container_status (empty string)
+def test_invalid_container_status(request_context, form_data):
+    form_data['container_status'] = ''  # Empty status
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_status' in form.errors
+
+# Test case for validating a form with an invalid iso_code (non-numeric)
+def test_invalid_iso_code(request_context, form_data):
+    form_data['iso_code'] = ''  
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'iso_code' in form.errors
+
+# Test case for validating a form with an invalid container_condition (incorrect spelling)
+def test_invalid_container_condition(request_context, form_data):
+    form_data['container_condtition'] = ''  
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'container_condtition' in form.errors
+
+# Test case for validating a form with an invalid date_of_manufacture (future date)
+def test_invalid_date_of_manufacture(request_context, form_data):
+    form_data['date_of_manufacture'] = ''  
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'date_of_manufacture' in form.errors
+
+# Test case for validating a form with an invalid last_date_inspection (before date_of_manufacture)
+def test_invalid_last_date_inspection(request_context, form_data):
+    form_data['last_date_inspection'] = '' 
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'last_date_inspection' in form.errors
+
+# Test case for validating a form with an invalid cargo_type (empty string)
+def test_invalid_cargo_type(request_context, form_data):
+    form_data['cargo_type'] = ''  
+    form = ContainerForm(data=form_data)
+    assert form.validate() is False
+    assert 'cargo_type' in form.errors
+
